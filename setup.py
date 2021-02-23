139723cafd2cf7abaaaa9c1eadf20e2a379f3d4c
pedrofale <pedro.ferreira@bsse.ethz.ch>
2021-02-23 14:55:10 +0000

    Don't add history to long description

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = [ ]
 
 setup_requirements = ['pytest-runner', ]
@@ -38,7 +35,7 @@ setup(
     },
     install_requires=requirements,
     license="GNU General Public License v3",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='scatrex',
     name='scatrex',
