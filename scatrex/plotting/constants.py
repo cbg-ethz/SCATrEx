38de8857b08406b1a3562f32451f37da163a5908
pedrofale <pedro.ferreira@bsse.ethz.ch>
2021-03-07 10:45:27 +0000

    Add plotting constants

---
+++ scatrex/plotting/constants.py
@@ -0,0 +1,5 @@
+import matplotlib
+
+CLONES_PAL = ["#7630A9", "#D55E00", "#E69F00", "#CC79A7", "#0072B2", "#56B4E9",  "#56DDA0"]
+BLUE_WHITE_RED = ["#2040C8", "white", "#EE241D"]
+CNV_CMAP = matplotlib.colors.LinearSegmentedColormap.from_list("cnv_cmap", BLUE_WHITE_RED)
