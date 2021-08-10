3be60057e55b7e1e3d0d0c605d71e0fdbf81a126
pedrofale <pedro.ferreira@bsse.ethz.ch>
2021-08-10 19:06:29 +0200

    Don't discard a priori empty clones

--- scatrex/scatrex.py
+++ scatrex/scatrex.py
@@ -139,7 +139,7 @@ class SCATrEx(object):
         if observed_tree:
             self.observed_tree = observed_tree
 
-        clones = np.array([self.observed_tree.tree_dict[clone]['params'] for clone in self.observed_tree.tree_dict if self.observed_tree.tree_dict[clone]['size'] > 0])
+        clones = np.array([self.observed_tree.tree_dict[clone]['params'] for clone in self.observed_tree.tree_dict])
 
         cell_idx = np.arange(self.adata.shape[0])
         others_idx = np.array([])
@@ -164,7 +164,7 @@ class SCATrEx(object):
         self.adata.obs['node'] = np.array([assignment.label for assignment in self.ntssb.assignments])
         self.adata.obs['obs_node'] = np.array([assignment.tssb.label for assignment in self.ntssb.assignments])
 
-        labels = [self.observed_tree.tree_dict[node]['label'] for node in self.observed_tree.tree_dict if self.observed_tree.tree_dict[node]['size'] > 0]
+        labels = [self.observed_tree.tree_dict[node]['label'] for node in self.observed_tree.tree_dict]
         sizes = [np.count_nonzero(self.adata.obs['obs_node']==label) for label in labels]
         self.adata.uns['estimated_frequencies'] = dict(zip(labels,sizes))
 
@@ -277,22 +277,26 @@ class SCATrEx(object):
             cell_idx = np.where(np.array([cell_filter in celltype for celltype in self.adata.obs['celltype_major']]))[0]
             others_idx = np.where(np.array([cell_filter not in celltype for celltype in self.adata.obs['celltype_major']]))[0]
 
-        labels = [self.observed_tree.tree_dict[clone]['label'] for clone in self.observed_tree.tree_dict if self.observed_tree.tree_dict[clone]['size'] > 0]
-        clones = [self.observed_tree.tree_dict[clone]['params'] for clone in self.observed_tree.tree_dict if self.observed_tree.tree_dict[clone]['size'] > 0]
+        labels = [self.observed_tree.tree_dict[clone]['label'] for clone in self.observed_tree.tree_dict]
+        clones = [self.observed_tree.tree_dict[clone]['params'] for clone in self.observed_tree.tree_dict]
         clones = np.array(clones)
         print(clones.shape)
 
         # Diploid clone
         dna_is_diploid = np.array((np.sum(clones == 2, axis=1) / clones.shape[1]) > dna_diploid_threshold)
-        diploid_clone_idx = np.where(dna_is_diploid)[0][0]
+        diploid_clone_indices = np.where(dna_is_diploid)[0]
         malignant_indices = np.arange(clones.shape[0])
-        malignant_indices_mask = np.ones(clones.shape[0], dtype=bool)
-        malignant_indices_mask[diploid_clone_idx] = 0
-        malignant_indices = malignant_indices[malignant_indices_mask]
-        diploid_labels = np.array(labels)[diploid_clone_idx].tolist()
-        malignant_labels = np.array(labels)[malignant_indices].tolist()
-        print(f'Diploid clones: {diploid_labels}')
-        print(f'Malignant clones: {malignant_labels}')
+        if len(diploid_clone_indices) == 0:
+            print("No diploid clones in scDNA-seq data.")
+        else:
+            diploid_clone_idx = diploid_clone_indices[0]
+            malignant_indices_mask = np.ones(clones.shape[0], dtype=bool)
+            malignant_indices_mask[diploid_clone_idx] = 0
+            malignant_indices = malignant_indices[malignant_indices_mask]
+            diploid_labels = np.array(labels)[diploid_clone_idx].tolist()
+            malignant_labels = np.array(labels)[malignant_indices].tolist()
+            print(f'Diploid clones: {diploid_labels}')
+            print(f'Malignant clones: {malignant_labels}')
 
         adata = self.adata.raw.to_adata()
         adata = adata[cell_idx]
@@ -355,9 +359,9 @@ class SCATrEx(object):
         self.adata.obs['node'] = assignments.astype(str)
         self.adata.obs['obs_node'] = assignments.astype(str)
 
-        labels = [self.observed_tree.tree_dict[node]['label'] for node in self.observed_tree.tree_dict if self.observed_tree.tree_dict[node]['size'] > 0]
-        sizes = [np.count_nonzero(self.adata.obs['obs_node']==label) for label in labels]
-        self.adata.uns['clonemap_estimated_frequencies'] = dict(zip(labels,sizes))
+        scdna_labels = [self.observed_tree.tree_dict[node]['label'] for node in self.observed_tree.tree_dict if self.observed_tree.tree_dict[node]['size'] > 0]
+        sizes = [np.count_nonzero(self.adata.obs['obs_node']==label) for label in scdna_labels]
+        self.adata.uns['clonemap_estimated_frequencies'] = dict(zip(scdna_labels,sizes))
 
         cnv_mat = np.ones(self.adata.shape) * 2
         for clone_id in np.unique(assignments):
