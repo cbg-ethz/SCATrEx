f0b5b36d64a087ae87d580d48924088ae50e6203
pedrofale <pedro.ferreira@bsse.ethz.ch>
2021-08-27 17:42:09 +0200

    Always keep the top kernel when merging

--- scatrex/ntssb/ntssb.py
+++ scatrex/ntssb/ntssb.py
@@ -1626,8 +1626,9 @@ class NTSSB(object):
             if numDataA > numDataB:
                 nodeB.variational_parameters['locals']['unobserved_factors_mean'] = nodeA.variational_parameters['locals']['unobserved_factors_mean']
                 nodeB.variational_parameters['locals']['unobserved_factors_log_std'] = nodeA.variational_parameters['locals']['unobserved_factors_log_std']
-                nodeB.variational_parameters['locals']['unobserved_factors_kernel_log_mean'] = nodeA.variational_parameters['locals']['unobserved_factors_kernel_log_mean']
-                nodeB.variational_parameters['locals']['unobserved_factors_kernel_log_std'] = nodeA.variational_parameters['locals']['unobserved_factors_kernel_log_std']
+                # We should always keep the top kernel
+                # nodeB.variational_parameters['locals']['unobserved_factors_kernel_log_mean'] = nodeA.variational_parameters['locals']['unobserved_factors_kernel_log_mean']
+                # nodeB.variational_parameters['locals']['unobserved_factors_kernel_log_std'] = nodeA.variational_parameters['locals']['unobserved_factors_kernel_log_std']
                 nodeB.variational_parameters['locals']['nu_log_mean'] = nodeA.variational_parameters['locals']['nu_log_mean']
                 nodeB.variational_parameters['locals']['nu_log_std'] = nodeA.variational_parameters['locals']['nu_log_std']
                 nodeB.variational_parameters['locals']['psi_log_mean'] = nodeA.variational_parameters['locals']['psi_log_mean']
