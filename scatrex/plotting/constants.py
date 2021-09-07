c0811ba135c129f444adbed02b1c85dfa9878c94
pedrofale <pedro.ferreira@bsse.ethz.ch>
2021-09-07 14:15:53 +0200

    Add two more colors to clones cmap

--- scatrex/plotting/constants.py
+++ scatrex/plotting/constants.py
@@ -1,6 +1,6 @@
 import matplotlib
 
-CLONES_PAL = ["#7630A9", "#D55E00", "#E69F00", "#CC79A7", "#0072B2", "#56B4E9",  "#56DDA0"]
+CLONES_PAL = ["#7630A9", "#D55E00", "#E69F00", "#CC79A7", "#0072B2", "#56B4E9",  "#56DDA0", "#49BD48", "#5E752B"]
 BLUE_WHITE_RED = ["#2040C8", "white", "#EE241D"]
 CNV_CMAP = matplotlib.colors.LinearSegmentedColormap.from_list("cnv_cmap", BLUE_WHITE_RED)
 PATHWAY_CMAP = matplotlib.cm.viridis
