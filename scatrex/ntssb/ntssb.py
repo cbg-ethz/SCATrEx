31ca26fe860c69c571671bf59ea45df475cfede2
pedrofale <pedro.ferreira@bsse.ethz.ch>
2021-10-19 12:40:28 +0100

    Adjust cell state when changing subtree

--- scatrex/ntssb/ntssb.py
+++ scatrex/ntssb/ntssb.py
@@ -1807,9 +1807,10 @@ class NTSSB(object):
 
         # Reset variational parameters: all log_std and unobserved factors kernel
         n_genes = target_subtree['node'].root['node'].cnvs.size
-        roots[nodeA_idx]['node'].variational_parameters['locals']['unobserved_factors_kernel_log_std'] = np.zeros((n_genes,))
+        roots[nodeA_idx]['node'].variational_parameters['locals']['unobserved_factors_mean'] = target_subtree['node'].root['node'].variational_parameters['locals']['unobserved_factors_mean']
         roots[nodeA_idx]['node'].variational_parameters['locals']['unobserved_factors_log_std'] = -1.*np.ones((n_genes,))
         roots[nodeA_idx]['node'].variational_parameters['locals']['unobserved_factors_kernel_log_mean'] = np.log(roots[nodeA_idx]['node'].unobserved_factors_kernel_concentration_caller())*np.ones((n_genes,))
+        roots[nodeA_idx]['node'].variational_parameters['locals']['unobserved_factors_kernel_log_std'] = -1*np.ones((n_genes,))
 
         # Set new unobserved factors to explain the same data as before (i.e. keep mean equal)
         # baseline = jnp.append(1, jnp.exp(self.root['node'].root['node'].log_baseline_caller()))
