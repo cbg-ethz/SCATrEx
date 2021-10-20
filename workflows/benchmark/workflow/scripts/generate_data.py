c92ef98572cab557be128172ffdf55e3d631059a
pedrofale <pedro.ferreira@bsse.ethz.ch>
2021-10-20 15:31:57 +0100

    Add more parameters to config

--- workflows/benchmark/workflow/scripts/generate_data.py
+++ workflows/benchmark/workflow/scripts/generate_data.py
@@ -4,6 +4,13 @@ n_genes = int(snakemake.params['n_genes'])
 n_cells = int(snakemake.params['n_cells'])
 n_extras = int(snakemake.params['n_extras'])
 n_factors = int(snakemake.params['n_factors'])
+n_regions = int(snakemake.params['n_regions'])
+min_nevents = int(snakemake.params['min_nevents'])
+max_nevents_frac = float(snakemake.params['max_nevents_frac'])
+frac_dosage = float(snakemake.params['frac_dosage'])
+log_lib_size_mean = float(snakemake.params['log_lib_size_mean'])
+log_lib_size_std = float(snakemake.params['log_lib_size_std'])
+
 simulated_data = snakemake.output['simulated_data']
 simulated_labels = snakemake.output['simulated_labels']
 simulated_clones = snakemake.output['simulated_clones']
@@ -13,12 +20,27 @@ simulated_observed_tree = snakemake.output['simulated_observed_tree']
 import scatrex
 from scatrex import models
 
-model_args = dict(log_lib_size_mean=10, num_global_noise_factors=n_factors, global_noise_factors_precisions_shape=10)
-sim_sca = scatrex.SCATrEx(model=models.cna, verbose=True, model_args=model_args)
-sim_sca.simulate_tree(n_genes=n_genes, n_extra_per_observed=n_extras, seed=seed)
+# model_args = dict(log_lib_size_mean=10, num_global_noise_factors=n_factors, global_noise_factors_precisions_shape=10)
+# sim_sca = scatrex.SCATrEx(model=models.cna, verbose=True, model_args=model_args)
+# sim_sca.simulate_tree(n_genes=n_genes, n_extra_per_observed=n_extras, seed=seed)
+# sim_sca.observed_tree.create_adata()
+# sim_sca.ntssb.reset_node_parameters(node_hyperparams=sim_sca.model_args)
+# data, labels = sim_sca.simulate_data(n_cells=n_cells, copy=True, seed=seed)
+
+theta = 50
+sim_sca = scatrex.SCATrEx(model=models.cna, verbose=True, model_args=dict(log_lib_size_mean=log_lib_size_mean, log_lib_size_std=log_lib_size_std,
+                                                                          num_global_noise_factors=4,
+                                                                          global_noise_factors_precisions_shape=10.,
+                                                                          unobserved_factors_kernel_concentration=1./theta,
+                                                                          frac_dosage=frac_dosage))
+observed_tree_args = dict(n_nodes=n_clones, node_weights=[1, 1, 1, 1])
+observed_tree_params = dict(n_regions=n_regions, min_cn=1, min_nevents=min_nevents, max_nevents_frac=max_nevents_frac)
+sim_sca.simulate_tree(observed_tree=None, n_extra_per_observed=n_extras, n_genes=n_genes, seed=seed,
+                        observed_tree_params=observed_tree_params, observed_tree_args=observed_tree_args)
 sim_sca.observed_tree.create_adata()
-sim_sca.ntssb.reset_node_parameters(node_hyperparams=sim_sca.model_args)
 data, labels = sim_sca.simulate_data(n_cells=n_cells, copy=True, seed=seed)
+sim_sca.plot_tree(counts=True)
+
 
 import numpy as np
 np.savetxt(simulated_data, data, delimiter=',')
--- workflows/benchmark/workflow/scripts/scatrex.py
+++ workflows/benchmark/workflow/scripts/scatrex.py
@@ -11,6 +11,7 @@ simulated_data = snakemake.input['simulated_data']
 simulated_clones = snakemake.input['simulated_clones']
 simulated_clones_labels = snakemake.input['simulated_clones_labels']
 simulated_observed_tree = snakemake.input['simulated_observed_tree']
+n_iters = snakemake.params['n_iters']
 n_tries = snakemake.params['n_tries']
 output_file = snakemake.output['fname']
 
@@ -18,10 +19,18 @@ adata = sc.read_csv(simulated_data)
 clones_adata = sc.read_csv(simulated_clones)
 clones_adata.obs['node'] = np.loadtxt(simulated_clones_labels, delimiter=',', dtype='str')
 
+
+theta = 50
+args = dict(global_noise_factors_precisions_shape=2, num_global_noise_factors=4,
+            unobserved_factors_kernel_concentration=1/theta,
+            unobserved_factors_kernel_rate=1.,
+            unobserved_factors_root_kernel=.01)
+
 sca_list = []
 for i in range(n_tries):
     # Create a new object for the inference
-    sca = scatrex.SCATrEx(model=models.cna, verbose=True)
+
+    sca = scatrex.SCATrEx(model=models.cna, verbose=True, model_args=args)
     sca.add_data(adata)
     sca.set_observed_tree(simulated_observed_tree)
 
@@ -42,7 +51,7 @@ for i in range(n_tries):
                     'full':.0,
                     'globals':1}
 
-    search_kwargs = {'n_iters': 500, 'n_iters_elbo': 500,
+    search_kwargs = {'n_iters': n_iters, 'n_iters_elbo': 500,
                     'move_weights': move_weights,
                     'local': True,
                     'factor_delay': 0,
@@ -53,7 +62,7 @@ for i in range(n_tries):
                     'window': 50,
                     'max_nodes': 5,
                     'add_rule_thres': .4,
-                    'joint_init': False}
+                    'joint_init': True}
     sca.learn_tree(reset=True, search_kwargs=search_kwargs)
     sca_list.append(sca)
 
