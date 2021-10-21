4265957af352933c019231e664a9df46d71a218b
pedrofale <pedro.ferreira@bsse.ethz.ch>
2021-10-21 16:13:46 +0100

    Change order of clone colors

--- scatrex/plotting/constants.py
+++ scatrex/plotting/constants.py
@@ -1,6 +1,5 @@
 import matplotlib
-
-CLONES_PAL = ["#7630A9", "#D55E00", "#E69F00", "#CC79A7", "#0072B2", "#56B4E9",  "#56DDA0", "#49BD48", "#5E752B", "#D4D948", "#A8722F", "#A83D2F", "#CF1F1F"]
+CLONES_PAL = ["#7630A9", "#CC79A7", "#D55E00", "#E69F00", "#0072B2", "#56B4E9",  "#56DDA0", "#49BD48", "#5E752B", "#D4D948", "#A8722F", "#A83D2F", "#CF1F1F"]
 BLUE_WHITE_RED = ["#2040C8", "white", "#EE241D"]
 CNV_CMAP = matplotlib.colors.LinearSegmentedColormap.from_list("cnv_cmap", BLUE_WHITE_RED)
 PATHWAY_CMAP = matplotlib.cm.viridis
