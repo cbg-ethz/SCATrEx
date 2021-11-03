0c65c93ef757c9b559aead53802400c2157af3ae
pedrofale <pedro.ferreira@bsse.ethz.ch>
2021-11-03 19:13:51 +0100

    Update benchmark plots

--- workflows/benchmark/workflow/scripts/plot_benchmark.R
+++ workflows/benchmark/workflow/scripts/plot_benchmark.R
@@ -2,33 +2,22 @@ suppressWarnings(library(tidyverse))
 suppressWarnings(library(ggpubr))
 
 inFile = snakemake@input[["fname"]]
-outFile = snakemake@output[["fname"]]
+ariFile = snakemake@output[["ari_fname"]]
+accFile = snakemake@output[["acc_fname"]]
 
 benchmark_df <- read.csv(inFile)
-extras <- as.character(as.integer(sort(unique(benchmark_df$n_extras))))
+extras <- as.character(as.integer(sort(unique(benchmark_df$n_extras)))+1)
 
-# Color library
-# colrys <- c("#ffc77c", "#CFB7A9", "#f2de8b", "#A7BBC9", "#F8766D", "#FF69B4", "#C77CFF", "#7cb4ff")
-# colors <- c("#ffd8a3", "#ffc77c", "#ffb655",
-#                "#ddccc2", "#CFB7A9", "#c1a290",
-#                "#f6e8ae", "#f2de8b", "#eed468",
-#                "#bfced8", "#A7BBC9", "#8fa8ba",
-#                "#cae1d4", "#a9cfba", "#88bda0",
-#                "#fa9992", "#F8766D", "#f65348",
-#                "#ff90c8", "#FF69B4", "#ff42a0",
-#                "#d8a3ff", "#C77CFF", "#b655ff",
-#                "#a3caff", "#7cb4ff", "#559eff")
-
-method_params <- list(correlation=list(name='Correlation', color=c("#ffd8a3", "#ffb655")),
-                      clonealign=list(name='clonealign', color=c("#ddccc2", "#c1a290")),
-                      leiden=list(name='leiden', color=c("#f6e8ae", "#eed468")),
-                      clonealign_leiden=list(name='clonealign+leiden', color=c("#cae1d4", "#88bda0")),
-                      scatrex=list(name='SCATrEx', color=c("#bfced8", "#8fa8ba")))
+method_params <- list(correlation=list(name='Correlation',newname='Correlation', color=c("#ffd8a3", "#ffb655", "#faa434")),
+                      clonealign=list(name='Clonealign', newname='Clonealign', color=c("#ddccc2", "#c1a290", "#d69874")),
+                      leiden=list(name='Leiden', newname='Leiden', color=c("#f6e8ae", "#eed468", "#f5cf38")),
+                      clonealign_leiden=list(name='Clonealign+Leiden', newname='Clonealign+\nLeiden', color=c("#cae1d4", "#88bda0", "#5ed192")),
+                      scatrex=list(name='SCATrEx', newname='SCATrEx', color=c("#bfced8", "#8fa8ba", "#699bbf")))
 get_colors <- function(method_list, method_params_list) {
   colors <- list()
   i = 1
-  for (method in names(method_params_list)){
-    if (method %in% method_list) {
+  for (method in method_list){
+    if (method %in% names(method_params_list)) {
       colors <- append(colors, method_params_list[[method]]$color)
     }
     i <- i + 1
@@ -37,79 +26,84 @@ get_colors <- function(method_list, method_params_list) {
   colors
 }
 
+get_names <- function(method_list, method_params_list) {
+  names <- list()
+  i = 1
+  for (method in method_list){
+    if (method %in% names(method_params_list)) {
+      names <- append(names, method_params_list[[method]]$name)
+    }
+    i <- i + 1
+  }
+  names <- unlist(names)
+  names
+}
+
+get_newnames <- function(method_list, method_params_list) {
+  names <- list()
+  i = 1
+  for (method in method_list){
+    if (method %in% names(method_params_list)) {
+      names <- append(names, method_params_list[[method]]$newname)
+    }
+    i <- i + 1
+  }
+  names <- unlist(names)
+  names
+}
+
+
+all_methods <- list('correlation', 'clonealign', 'leiden', 'clonealign_leiden', 'scatrex')
+all_colors <- get_colors(all_methods, method_params)
+all_names <- get_names(all_methods, method_params)
+
+
 ptitle = 'Clone assignment accuracy'
 score = 'clone_accuracy'
 methods = c('correlation', 'clonealign', 'scatrex')
+names <- get_names(methods, method_params)
+newnames <- get_newnames(methods, method_params)
 colors <- get_colors(methods, method_params)
 benchmark_df_subset <- benchmark_df[benchmark_df$score == score,]
 benchmark_df_subset <- benchmark_df_subset[benchmark_df_subset$method %in% methods,]
 benchmark_df_subset$method <- factor(benchmark_df_subset$method , levels=methods)
 p1 <- ggplot(benchmark_df_subset, aes(x = method, y = value, color = interaction(n_extras, method), fill = interaction(n_extras, method))) +
   geom_boxplot(outlier.shape = NA) +
-  geom_jitter(shape=16, position=position_jitterdodge(), alpha=0.5) +
+  geom_jitter(shape=16, position=position_jitterdodge(), alpha=0.6,size=2) +
   xlab(NULL) +
   labs(y=ptitle) +
-  scale_color_manual(values=colors, name= " \n ", labels=paste0(rep(methods, each=length(extras)), "\n", extras, " extra/clone")) +
-  scale_fill_manual(values=paste0(colors, "80"), name= " \n ", labels=paste0(rep(methods, each=length(extras)), "\n", extras, " extra/clone")) +
+  scale_x_discrete(labels=newnames) +
+  scale_color_manual(values=colors, name= " \n ", labels=paste0(rep(names, each=length(extras)), "\n", extras, " nodes/clone"), drop=FALSE) +
+  scale_fill_manual(values=paste0(colors, "80"), name= " \n ", labels=paste0(rep(names, each=length(extras)), "\n", extras, " nodes/clone"), drop=FALSE) +
+  geom_hline(yintercept=.25, linetype="dashed", color = "gray") +
+  guides(fill=guide_legend(ncol=2), color=guide_legend(ncol=2)) +
   theme_bw(base_size = 16) + theme(legend.key.height = unit(1.5, "cm")) +
   theme(axis.text.x=element_text(angle=45,hjust=1,size=12)) +
-  facet_wrap(~n_factors, labeller=label_bquote(paste(.(n_factors)," factors")),scales = "fixed", nrow = 1) #+
+  facet_wrap(~n_factors, labeller=label_bquote(paste(.(n_factors)," noise factors")),scales = "fixed", nrow = 1) #+
+ggsave(accFile, width=15, height=4.5, dpi=200, bg = "transparent", )
 
-ptitle = 'Clone-level v-measure'
-score = 'clone_ari'
+all_methods <- c('leiden', 'clonealign_leiden', 'scatrex')
+all_colors <- get_colors(all_methods, method_params)
+all_names <- get_names(all_methods, method_params)
+ptitle = 'Node adjusted rand score'
+score = 'node_ari'
 methods = c('leiden', 'clonealign_leiden', 'scatrex')
+names <- get_names(methods, method_params)
+newnames <- get_newnames(methods, method_params)
 colors <- get_colors(methods, method_params)
 benchmark_df_subset <- benchmark_df[benchmark_df$score == score,]
 benchmark_df_subset <- benchmark_df_subset[benchmark_df_subset$method %in% methods,]
 benchmark_df_subset$method <- factor(benchmark_df_subset$method , levels=methods)
 p2 <- ggplot(benchmark_df_subset, aes(x = method, y = value, color = interaction(n_extras, method), fill = interaction(n_extras, method))) +
   geom_boxplot(outlier.shape = NA) +
-  geom_jitter(shape=16, position=position_jitterdodge(), alpha=0.5) +
+  geom_jitter(shape=16, position=position_jitterdodge(), alpha=0.6, size=2) +
   xlab(NULL) +
   labs(y=ptitle) +
-  scale_color_manual(values=colors, name= " \n ", labels=paste0(rep(methods, each=length(extras)), "\n", extras, " extra/clone")) +
-  scale_fill_manual(values=paste0(colors, "80"), name= " \n ", labels=paste0(rep(methods, each=length(extras)), "\n", extras, " extra/clone")) +
+  scale_color_manual(values=colors, name= " \n ", labels=paste0(rep(newnames, each=length(extras)), "\n", extras, " nodes/clone")) +
+  scale_fill_manual(values=paste0(colors, "80"), name= " \n ", labels=paste0(rep(newnames, each=length(extras)), "\n", extras, " nodes/clone")) +
+  scale_x_discrete(labels=newnames) +
   theme_bw(base_size = 16) + theme(legend.key.height = unit(1.5, "cm")) +
+  guides(fill=guide_legend(ncol=2), color=guide_legend(ncol=2)) +
   theme(axis.text.x=element_text(angle=45,hjust=1,size=12)) +
-  facet_wrap(~n_factors, labeller=label_bquote(paste(.(n_factors)," factors")),scales = "fixed", nrow = 1) #+
-
-ptitle = 'Node-level v-measure'
-score = 'node_ari'
-methods = c('leiden', 'clonealign_leiden', 'scatrex')
-colors <- get_colors(methods, method_params)
-benchmark_df_subset <- benchmark_df[benchmark_df$score == score,]
-benchmark_df_subset <- benchmark_df_subset[benchmark_df_subset$method %in% methods,]
-benchmark_df_subset$method <- factor(benchmark_df_subset$method , levels=methods)
-p3 <- ggplot(benchmark_df_subset, aes(x = method, y = value, color = interaction(n_extras, method), fill = interaction(n_extras, method))) +
-  geom_boxplot(outlier.shape = NA) +
-  geom_jitter(shape=16, position=position_jitterdodge(), alpha=0.5) +
-  xlab(NULL) +
-  labs(y=ptitle) +
-  scale_color_manual(values=colors, name= " \n ", labels=paste0(rep(methods, each=length(extras)), "\n", extras, " extra/clone")) +
-  scale_fill_manual(values=paste0(colors, "80"), name= " \n ", labels=paste0(rep(methods, each=length(extras)), "\n", extras, " extra/clone")) +
-  theme_bw(base_size = 16) + theme(legend.key.height = unit(1.5, "cm")) +
-  theme(axis.text.x=element_text(angle=45,hjust=1,size=12)) +
-  facet_wrap(~n_factors, labeller=label_bquote(paste(.(n_factors)," factors")),scales = "fixed", nrow = 1) #+
-
-ptitle = 'Node assignment accuracy'
-score = 'node_accuracy'
-methods = c('scatrex')
-colors <- get_colors(methods, method_params)
-benchmark_df_subset <- benchmark_df[benchmark_df$score == score,]
-benchmark_df_subset <- benchmark_df_subset[benchmark_df_subset$method %in% methods,]
-benchmark_df_subset$method <- factor(benchmark_df_subset$method , levels=methods)
-p4 <- ggplot(benchmark_df_subset, aes(x = method, y = value, color = interaction(n_extras, method), fill = interaction(n_extras, method))) +
-  geom_boxplot(outlier.shape = NA) +
-  geom_jitter(shape=16, position=position_jitterdodge(), alpha=0.5) +
-  xlab(NULL) +
-  labs(y=ptitle) +
-  scale_color_manual(values=colors, name= " \n ", labels=paste0(rep(methods, each=length(extras)), "\n", extras, " extra/clone")) +
-  scale_fill_manual(values=paste0(colors, "80"), name= " \n ", labels=paste0(rep(methods, each=length(extras)), "\n", extras, " extra/clone")) +
-  theme_bw(base_size = 16) + theme(legend.key.height = unit(1.5, "cm")) +
-  theme(axis.text.x=element_text(angle=45,hjust=1,size=12)) +
-  facet_wrap(~n_factors, labeller=label_bquote(paste(.(n_factors)," factors")),scales = "fixed", nrow = 1) #+
-
-figure <- ggarrange(p1, p2, p3, p4,
-                    labels = c("A", "B", "C", "D"),
-                    ncol = 1, nrow = 4)
-ggsave(outFile, width=15, height=16, dpi=200, bg = "transparent")
+  facet_wrap(~n_factors, labeller=label_bquote(paste(.(n_factors)," noise factors")),scales = "fixed", nrow = 1) #+
+ggsave(ariFile, width=15, height=4.5, dpi=200, bg = "transparent", )
