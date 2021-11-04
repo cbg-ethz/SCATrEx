f8abfaa46c5895af1a9a6816a51b4ad677f45a08
pedrofale <pedro.ferreira@bsse.ethz.ch>
2021-11-04 11:22:43 +0100

    Bump version to 0.0.3

--- scatrex/__init__.py
+++ scatrex/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Pedro Fale Ferreira"""
 __email__ = 'pedro.miguel.ferreira.pf@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.0.3'
 
 from .scatrex import SCATrEx
 from .models import *
