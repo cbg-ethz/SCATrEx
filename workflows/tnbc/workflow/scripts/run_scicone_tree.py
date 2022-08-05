d08174beb8fdbd4b8cd8494343e14a85856cb99f
pedrofale <pedro.ferreira@bsse.ethz.ch>
2022-08-05 22:52:32 +0200

    Lint code

--- workflows/tnbc/workflow/scripts/run_scicone_tree.py
+++ workflows/tnbc/workflow/scripts/run_scicone_tree.py
@@ -13,22 +13,30 @@ best_tree_txt_path = snakemake.output["best_tree_txt_path"]
 seed = 42
 
 # Load data
-data = np.loadtxt(data_file, delimiter=',')
-segmented_region_sizes = np.loadtxt(segmented_region_sizes, delimiter=',')
+data = np.loadtxt(data_file, delimiter=",")
+segmented_region_sizes = np.loadtxt(segmented_region_sizes, delimiter=",")
 
 # Run cluster tree
-sci = scicone.SCICoNE(binary_path=scicone_path,
-                        output_path="",
-                        persistence=False,
-                        )
+sci = scicone.SCICoNE(
+    binary_path=scicone_path,
+    output_path="",
+    persistence=False,
+)
 
-inferred_tree = sci.learn_tree(data, segmented_region_sizes=segmented_region_sizes,
-                         n_reps=n_reps, max_tries=n_tries, cluster_tree_n_iters=n_iters, full=False, seed=seed)
+inferred_tree = sci.learn_tree(
+    data,
+    segmented_region_sizes=segmented_region_sizes,
+    n_reps=n_reps,
+    max_tries=n_tries,
+    cluster_tree_n_iters=n_iters,
+    full=False,
+    seed=seed,
+)
 
 # Save best tree
-with open(best_tree_txt_path, 'w') as f:
-        f.write(inferred_tree.tree_str)
+with open(best_tree_txt_path, "w") as f:
+    f.write(inferred_tree.tree_str)
 
 # Save pickled object too
-with open(best_tree_pkl_path, 'w') as f:
-        pickle.dump(inferred_tree, f)
+with open(best_tree_pkl_path, "w") as f:
+    pickle.dump(inferred_tree, f)
